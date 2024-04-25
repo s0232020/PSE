a8bf52f204fca92bf03c8dfbae7f712092b82e73
Jelle <jelle.jacobs@student.uantwerpen.be>
2024-04-25 18:15:14 +0200

    commit
    Use case 3.5 finished

--- src/PrintingSystem.h
+++ src/PrintingSystem.h
@@ -99,10 +99,16 @@ public:
         }
     }
 
+    void addUncompletedJob(Job& job)
+    {
+        uncompletedJobs.emplace_back(job);
+    }
+
 private:
     std::vector<Printer> printers_;
     std::vector<Job> jobs_;
     std::vector<ClimateCompensationInitiative> climateCompensationInitiatives_;
+    std::vector<Job> uncompletedJobs;
 
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     PrintingSystem system;
-    system.loadFromFile("valid.xml");
+    system.loadFromFile("missing_device_for_job_type.xml");
     system.addJobsToPrinters(system);
     system.generateStatusReport("valid_output.txt");
     system.processJob("Office_Printer5");
